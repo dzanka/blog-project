28c4e15a95e916fe12336ed243f6535aa038d547
dzanka <dadova.jana@gmail.com>
2020-10-18 23:51:12 +0200

    adds get, input post, delete requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ErrorBoundary from './hoc/ErrorBoundary';
 
 import Blog from './containers/Blog/Blog';
 
@@ -6,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Blog />
+        <ErrorBoundary>
+          <Blog />
+        </ErrorBoundary>
       </div>
     );
   }
