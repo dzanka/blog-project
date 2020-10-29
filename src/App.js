de3ce66d5dd69a1533a0d7392fbabb0462786115
dzanka <dadova.jana@gmail.com>
2020-10-29 09:26:05 +0100

    move files to posts folder to prepare for routing  v.192

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import ErrorBoundary from './hoc/ErrorBoundary';
+import { BrowserRouter } from 'react-router-dom';
+//import ErrorBoundary from './hoc/ErrorBoundary';
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ErrorBoundary>
-          <Blog />
-        </ErrorBoundary>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+            <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
